6a236aa308590c10b56f3919a0852d1c81be91db
Jacob Ashkenas <jacobashkenas@gmail.com>
2022-07-19 20:08:32 -0400

    Register popups

--- components/popUp.js
+++ components/popUp.js
@@ -3,7 +3,7 @@ import styles from '@styles/PopUp.module.css';
 export default function PopUp(props) {
     return (
         <div className={`${styles.popup} ${props.flipY ? styles.flipY : ''} ${props.flipX ? styles.flipX : ''}`}>
-            <h3 className={styles.title}>{props.title}</h3>
+            {props.title && <h3 className={styles.title}>{props.title}</h3>}
             <div className={styles.body}>
                 {props.children}
             </div>
--- components/register.js
+++ components/register.js
@@ -1,40 +1,36 @@
 import React from 'react';
 import { bigIntToHexString } from '@util/formatUtils';
 import styles from '@styles/Register.module.css';
+import PopUp from './popUp';
 
-class Register extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+// setBit(bit, value) {
+//     if (value > 1 || value < 0)
+//         throw `Bit value must be '0' or '1'!`;
+    
+//     if (typeof value !== 'bigint')
+//         value = BigInt(value);
 
-    setBit(bit, value) {
-        if (value > 1 || value < 0)
-            throw `Bit value must be '0' or '1'!`;
-        
-        if (typeof value !== 'bigint')
-            value = BigInt(value);
+//     if (typeof bit !== 'bigint')
+//         bit = BigInt(bit);
+    
+//     const mask = (1n << 65n) - 1n - (1n << bit);
+//     this.setValue((this.getValue() & mask) | (value << bit))
+// }
 
-        if (typeof bit !== 'bigint')
-            bit = BigInt(bit);
-        
-        const mask = (1n << 65n) - 1n - (1n << bit);
-        this.setValue((this.getValue() & mask) | (value << bit))
-    }
+// getBit(bit) {
+//     return (this.state.value >> BigInt(bit)) & 1n;
+// }
 
-    getBit(bit) {
-        return (this.state.value >> BigInt(bit)) & 1n;
-    }
-
-    render() {
-        return (
-            <div className={styles.register}>
-                {this.props.registerName}:
-                <span className={styles.value}>
-                    {bigIntToHexString(this.props.value < 0 ? (1n << 64n) + this.props.value : this.props.value, 64)}
-                </span>
-            </div>
-        );
-    }
+export default function Register(props) {
+    return (
+        <div className={styles.register}>
+            {props.registerName}:
+            <span className={styles.value}>
+                <PopUp title={props.registerName}>
+                    {props.value.toString()}
+                </PopUp>
+                {bigIntToHexString(props.value < 0 ? (1n << 64n) + props.value : props.value, 64)}
+            </span>
+        </div>
+    );
 }
-
-export default Register;

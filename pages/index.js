7328c572820724a9bc6fd61e039c711f1e239570
Jacob Ashkenas <jacobashkenas@gmail.com>
2022-07-27 18:56:58 -0400

    Redux "added"

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import Simulator from "@arch/simulator";
 import ErrorBoundary from "@components/errorBoundary";
+import { Provider} from "react-redux";
+import { applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+
+const store = createStore(() => {}, undefined, applyMiddleware(thunkMiddleware));
 
 export default function Home() {
     return (
-        <ErrorBoundary title={"Runtime Error"}>
-            <Simulator />
-        </ErrorBoundary>
+        <Provider store={store}>
+            <ErrorBoundary title={"Runtime Error"}>
+                <Simulator />
+            </ErrorBoundary>
+        </Provider>
     );
 }
